3e4da7e8e62588002bbbb47e9f7bba9d6fd28781
ruviRedner <r.y.r7966@gmail.com>
2024-11-10 15:09:06 +0200

    add slash

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,16 @@ import userController from "./controllers/userController"
 import adminController from "./controllers/admin"
 import candidatesController from "./controllers/candidates"
 import votesController from "./controllers/votes"
+import { connectToMongo } from "./config/db"
 const PORT = process.env.PORT || 3000
 
 const app = exp()
 app.use(exp.json())
-app.use("api/users",userController)
-app.use("api/admin",adminController)
-app.use("api/candidates",candidatesController)
-app.use("api/votes",votesController)
+connectToMongo()
+app.use("/api/users",userController)
+app.use("/api/admin",adminController)
+app.use("/api/candidates",candidatesController)
+app.use("/api/votes",votesController)
 
 
 
