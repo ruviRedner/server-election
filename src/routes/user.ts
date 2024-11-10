72441a0169108fce694a22319d429cd0ee484e14
ruviRedner <r.y.r7966@gmail.com>
2024-11-10 15:15:47 +0200

    login and register done

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,17 +1,27 @@
 import { Request, Response } from "express";
+import { LoginDto, RegisterDto } from "../dto/userDto";
+import { userLogin, userRegister } from "../services/user";
 
-export const login = async (req:Request,res:Response):Promise<void> =>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+export const login = async (
+  req: Request<LoginDto>,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await userLogin(req.body);
+    res.status(200).json({result});
+  } catch (error) {
+    res.status(400).json((error as Error).message);
+  }
+};
 
-export const register = async (req:Request,res:Response):Promise<void> =>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+export const register = async (
+  req: Request<RegisterDto>,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await userRegister(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json((error as Error).message);
+  }
+};
