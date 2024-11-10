176ebfb35a9cb1e17cd71449f6506e1ac11ef9c7
ruviRedner <r.y.r7966@gmail.com>
2024-11-10 11:57:18 +0200

    bssic func

---
+++ src/routes/user.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const login = async (req:Request,res:Response):Promise<void> =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const register = async (req:Request,res:Response):Promise<void> =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
