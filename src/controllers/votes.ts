6b01846c4db87c0de760f3348c11e2f93350f225
ruviRedner <r.y.r7966@gmail.com>
2024-11-11 12:36:19 +0200

    add route for vote

--- src/controllers/votes.ts
+++ src/controllers/votes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import verifyUser from "../middlewares/verifyUser";
+import { vote } from "../routes/votes";
 
 const router = Router()
+
+router.post("/",verifyUser,vote)
 
 export default router
