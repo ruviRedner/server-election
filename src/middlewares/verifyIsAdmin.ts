1da7896284e98952d08e97ee4ef77b9e9ccf13a7
ruviRedner <r.y.r7966@gmail.com>
2024-11-11 10:32:27 +0200

    add verify is admin

---
+++ src/middlewares/verifyIsAdmin.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+
+export default (req:Request,res:Response,next:NextFunction) =>{
+    try {
+        const token = req.headers["authorization"]
+        if(!token) {
+            res.status(401).json({err:"Token must be provider"})
+            return
+        } 
+       const payload = jwt.verify(token,process.env.JWT_SECRET!);
+       (req as any).user = payload
+       if(!(payload as any).isAdmin){
+        res.status(403).json({err:"Only admin can access this route"})
+       }
+       next()
+    } catch (error) {
+        res.status(401).json(
+        error as JsonWebTokenError)
+    }
+}
