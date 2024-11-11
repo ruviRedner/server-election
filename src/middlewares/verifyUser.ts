4cd15ba48d326193967e5f1401485916d94d7e44
ruviRedner <r.y.r7966@gmail.com>
2024-11-11 10:24:19 +0200

    middleware verify user done

---
+++ src/middlewares/verifyUser.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+
+export default (req:Request,res:Response,next:NextFunction) =>{
+    try {
+        const token = req.headers["authorization"]
+        if(!token) {
+            res.status(401).json({err:"Token must be provider"})
+            return
+        } 
+       const payload = jwt.verify(token,process.env.JWT_SECRET!);
+       (req as any).user = payload
+       next()
+    } catch (error) {
+        res.status(401).json(
+        error as JsonWebTokenError)
+    }
+}
