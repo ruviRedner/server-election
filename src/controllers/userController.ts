7bac6d24db97ae4a786233813fc0d3b2e5fc06e2
ruviRedner <r.y.r7966@gmail.com>
2024-11-11 16:55:39 +0200

    activate get profile in controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { login, register } from "../routes/user";
+import { getProfile, login, register } from "../routes/user";
+import verifyUser from "../middlewares/verifyUser";
 
 const router = Router()
 
 router.post("/login",login)
 
 router.post("/register",register)
+
+router.get("/profile",verifyUser,getProfile)
 
 export default router
