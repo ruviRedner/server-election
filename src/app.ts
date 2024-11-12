ff45cc3b3ec41e54fd941db82b5cc9a0b137d24c
ruviRedner <r.y.r7966@gmail.com>
2024-11-12 12:21:41 +0200

    activate socket

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,34 @@
-import exp from "express"
-import "dotenv/config"
-import userController from "./controllers/userController"
-import adminController from "./controllers/admin"
-import candidatesController from "./controllers/candidates"
-import votesController from "./controllers/votes"
-import { connectToMongo } from "./config/db"
-import cors from "cors"
-import http from "http"
-import {Server} from "socket.io"
-const PORT = process.env.PORT || 3000
+import exp from "express";
+import "dotenv/config";
+import userController from "./controllers/userController";
+import adminController from "./controllers/admin";
+import candidatesController from "./controllers/candidates";
+import votesController from "./controllers/votes";
+import { connectToMongo } from "./config/db";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import { handelShackConnection } from "./socket/io";
+const PORT = process.env.PORT || 3000;
 
-const app = exp()
-const httpServer = http.createServer(app)
-export const io = new Server(httpServer)
+const app = exp();
+const httpServer = http.createServer(app);
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
+io.on("connection", handelShackConnection);
 
-app.use(exp.json())
-app.use(cors())
-connectToMongo()
-app.use("/api/users",userController)
-app.use("/api/admin",adminController)
-app.use("/api/candidates",candidatesController)
-app.use("/api/votes",votesController)
+app.use(exp.json());
+app.use(cors());
+connectToMongo();
+app.use("/api/users", userController);
+app.use("/api/admin", adminController);
+app.use("/api/candidates", candidatesController);
+app.use("/api/votes", votesController);
 
-
-
-httpServer.listen(PORT,()=>{
-    console.log(`server app and runing in port ${PORT}`);
-    
-})
+httpServer.listen(PORT, () => {
+  console.log(`server app and runing in port ${PORT}`);
+});
