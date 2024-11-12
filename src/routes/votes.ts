1853640514f5e8f4667d24e3370f0b604667dfa6
ruviRedner <r.y.r7966@gmail.com>
2024-11-12 19:26:55 +0200

    creating path for unvote done

--- src/routes/votes.ts
+++ src/routes/votes.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
-import { handelVote } from "../services/votes";
+import { handelUnVote, handelVote } from "../services/votes";
 import { Vote } from "../interfaces/Vote";
 
 export const vote = async (req:Request<any,any,Vote>,res:Response) => {
     try {
        const result = await handelVote(req.body)
+        res.json({result})
+    } catch (error) {
+        res.status(500).json({error})
+    }
+
+}
+export const unVote = async (req:Request<any,any,Vote>,res:Response) => {
+    try {
+       const result = await handelUnVote(req.body)
+       
         res.json({result})
     } catch (error) {
         res.status(500).json({error})
