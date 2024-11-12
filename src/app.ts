36245438e0e821ef9f88b4c55160016dae7ff194
ruviRedner <r.y.r7966@gmail.com>
2024-11-12 11:53:05 +0200

    server without express

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ import candidatesController from "./controllers/candidates"
 import votesController from "./controllers/votes"
 import { connectToMongo } from "./config/db"
 import cors from "cors"
+import http from "http"
 const PORT = process.env.PORT || 3000
 
 const app = exp()
+const httpServer = http.createServer(app)
 app.use(exp.json())
 app.use(cors())
 connectToMongo()
